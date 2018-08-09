5f5a1ad06dd49cf2bae32c9bf30e92bb99c391f3
Tisho Instrospective <MitkovTihomir@gmail.com>
2018-08-09 20:17:17 +0300

    Adding another (colorize) cube which only rotates

--- cube-1.c
+++ cube-1.c
@@ -14,6 +14,52 @@ void init(void){
   glShadeModel(GL_FLAT);
 }
 
+
+void drawCube() {
+    glLoadIdentity();
+    glTranslatef(1.5f, 0.0f, -7.0f);
+    glRotatef(rotate, 1.0f, 1.0f, 1.0f);
+    glBegin(GL_QUADS);
+
+    glColor3f(0.0f,1.0f,0.0f); // Set The Color To Green
+    glVertex3f( 1.0f, 1.0f,-1.0f); // Top Right Of The Quad (Top)
+    glVertex3f(-1.0f, 1.0f,-1.0f); // Top Left Of The Quad (Top)
+    glVertex3f(-1.0f, 1.0f, 1.0f); // Bottom Left Of The Quad (Top)
+    glVertex3f( 1.0f, 1.0f, 1.0f); // Bottom Right Of The Quad (Top)
+
+    glColor3f(1.0f,0.5f,0.0f); // Set The Color To Orange
+    glVertex3f( 1.0f,-1.0f, 1.0f); // Top Right Of The Quad (Bottom)
+    glVertex3f(-1.0f,-1.0f, 1.0f); // Top Left Of The Quad (Bottom)
+    glVertex3f(-1.0f,-1.0f,-1.0f); // Bottom Left Of The Quad (Bottom)
+    glVertex3f( 1.0f,-1.0f,-1.0f); // Bottom Right Of The Quad (Bottom)
+
+    glColor3f(1.0f,0.0f,0.0f); // Set The Color To Red
+    glVertex3f( 1.0f, 1.0f, 1.0f); // Top Right Of The Quad (Front)
+    glVertex3f(-1.0f, 1.0f, 1.0f); // Top Left Of The Quad (Front)
+    glVertex3f(-1.0f,-1.0f, 1.0f); // Bottom Left Of The Quad (Front)
+    glVertex3f( 1.0f,-1.0f, 1.0f); // Bottom Right Of The Quad (Front)
+
+    glColor3f(1.0f,1.0f,0.0f); // Set The Color To Yellow
+    glVertex3f( 1.0f,-1.0f,-1.0f); // Bottom Left Of The Quad (Back)
+    glVertex3f(-1.0f,-1.0f,-1.0f); // Bottom Right Of The Quad (Back)
+    glVertex3f(-1.0f, 1.0f,-1.0f); // Top Right Of The Quad (Back)
+    glVertex3f( 1.0f, 1.0f,-1.0f); // Top Left Of The Quad (Back)
+
+    glColor3f(0.0f,0.0f,1.0f); // Set The Color To Blue
+    glVertex3f(-1.0f, 1.0f, 1.0f); // Top Right Of The Quad (Left)
+    glVertex3f(-1.0f, 1.0f,-1.0f); // Top Left Of The Quad (Left)
+    glVertex3f(-1.0f,-1.0f,-1.0f); // Bottom Left Of The Quad (Left)
+    glVertex3f(-1.0f,-1.0f, 1.0f); // Bottom Right Of The Quad (Left)
+
+    glColor3f(1.0f,0.0f,1.0f); // Set The Color To Violet
+    glVertex3f( 1.0f, 1.0f,-1.0f); // Top Right Of The Quad (Right)
+    glVertex3f( 1.0f, 1.0f, 1.0f); // Top Left Of The Quad (Right)
+    glVertex3f( 1.0f,-1.0f, 1.0f); // Bottom Left Of The Quad (Right)
+    glVertex3f( 1.0f,-1.0f,-1.0f); // Bottom Right Of The Quad (Right)
+    glEnd(); // Done Drawing The Quad
+    //return TRUE; // Keep Going
+}
+
 void display(void){
   glClear(GL_COLOR_BUFFER_BIT);
   glColor3f(1.0, 1.0, 1.0);
@@ -26,6 +72,8 @@ void display(void){
   glScalef(1.0, 2.0, 1.0);
   /* modeling transformation */
   glutWireCube(1.0);
+
+  drawCube();
   glFlush();
 }
 
