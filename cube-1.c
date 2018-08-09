b4153c5fdd040a90e78b886a572684a2da2fe068
Tisho Instrospective <MitkovTihomir@gmail.com>
2018-08-09 19:43:06 +0300

    recalculating scene in a separate thread

--- cube-1.c
+++ cube-1.c
@@ -1,6 +1,12 @@
 #include <GL/glut.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <time.h>
 
 static GLfloat offset = -1.0f;
+
+pthread_t thread;
 
 void init(void){
   glClearColor(0.0, 0.0, 0.0, 0.0);
@@ -31,22 +37,41 @@ void reshape(int w, int h){
 }
 
 void spinMatrix(void){
-  if(offset > -12.0){
-    offset = offset - 0.00008f;
+    offset = offset - 0.008f;
     glutPostRedisplay();
-  }
+    printf("%f\n", offset);
+}
+
+void* threadProc(void* arg) {
+    while(1) {
+        if(offset > -12.0){
+            spinMatrix();
+            usleep(1000 * 30); //300 milliseconds
+        }
+        else {
+            break;
+        }
+    }
 }
 
 int main(int argc, char** argv){
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-  glutInitWindowSize(500, 500);
-  glutInitWindowPosition(100, 100);
-  glutCreateWindow(argv[0]);
-  init();
-  glutDisplayFunc(display);
-  glutReshapeFunc(reshape);
-  glutIdleFunc(spinMatrix);
-  glutMainLoop();
-  return 0;
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow(argv[0]);
+    init();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    //glutIdleFunc(spinMatrix);
+
+    // here we create and let a thread run
+    // in the background calculating next
+    // frame every now and then and letting
+    // glut know that it's time to redraw
+    // scene
+    pthread_create(&thread, NULL, threadProc, NULL);
+
+    glutMainLoop();
+    return 0;
 }
